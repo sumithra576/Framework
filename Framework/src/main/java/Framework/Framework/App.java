666d64f41ce6868af912a3cdd157b212cef18d31
Sumithra <sumithra576@gmail.com>
2020-12-21 16:05:45 -0500

    commit2

--- Framework/src/main/java/Framework/Framework/App.java
+++ Framework/src/main/java/Framework/Framework/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("One commit one friendship");
+        
         System.out.println("Comparison betwee the frameworks0");
     }
 }
